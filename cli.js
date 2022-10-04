580cf63fdc3439dd0783cbe85542691919e4a496
Abigail Marlett <amarlett@email.unc.edu>
2022-10-04 12:00:22 -0400

    add

--- cli.js
+++ cli.js
@@ -13,16 +13,20 @@ var jsononly = false
 
 
 //help text
-if (args.h){
-    console.log(`Usage: cli.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+if (args.h) {
+	console.log(
+	`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.`)
-}
+    -j            Echo pretty JSON from open-meteo API and exit.
+	`);
+	process.exit(0);
+};
 
+//if statements for arguments 
 if (args.n){
     var lat = args.n
 }
@@ -45,6 +49,7 @@ if (args.j){
     var jsononly = true
 }
 
+//fetch
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+lon+'&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone='+timezone+'&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant');
 const data = await response.json()
 if(jsononly){
@@ -52,8 +57,8 @@ if(jsononly){
     process.exit(0)
 }
 
-const days = args.d 
 
+//how many days?
 if (days == 0) {
     var text = "today."
   } else if (days > 1) {
@@ -62,7 +67,8 @@ if (days == 0) {
     var text = "tomorrow."
   }
 
-  var precip = data.daily.precipitation_hours[day]
+const days = args.d 
+var precip = data.daily.precipitation_hours[day]
   
 if(prec==0){
     console.log("You will not need your galoshes "+text)
